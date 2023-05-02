544d4195c88276bba6de31dee6ed38fee3593644
JulianNieva <juliannieva90@gmail.com>
2023-05-02 18:34:07 -0300

    Agregado primera parte terminada

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { BienvenidoComponent } from './componentes/bienvenido/bienvenido.component';
+import { BusquedaComponent } from './componentes/busqueda/busqueda.component';
+import { ActorAltaComponent } from './componentes/actor/actor-alta/actor-alta.component';
+import { PeliculaListadoComponent } from './componentes/peliculas/pelicula-listado/pelicula-listado.component';
+import { ActorListadoComponent } from './componentes/actor/actor-listado/actor-listado.component';
+import { PeliculaAltaComponent } from './componentes/peliculas/peliculas-alta/pelicula-alta.component';
+
+const routes: Routes = [
+  {path:'bienvenido', component:BienvenidoComponent},
+  {path:'busqueda', component:BusquedaComponent},
+  {path:'peliculas/alta', component:PeliculaAltaComponent},
+  {path:'actor/alta', component:ActorAltaComponent},
+  {path:'actor/listado', component:ActorListadoComponent},
+  {path:'peliculas/listado', component:PeliculaListadoComponent},
+  {path:'', component:BienvenidoComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
